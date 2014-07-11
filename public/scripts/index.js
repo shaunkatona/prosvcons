bea28428b9f9c5d124f94e4d4ed59e415eb0de4f
shaunkatona <shaunkatona@Shauns-MacBook-Pro.local>
2014-07-11 12:13:17 -0600

    remove query events.  angular's got it covered.

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -5,10 +5,4 @@
 $(function() {
     // start the cursor out on the pro textbox
     $("#pro").focus();
-
-    // deleting a pro or a con
-    $(".prosAndCons").on("click", "a.delete", function (e) {
-        var $lineItem = $(this).closest("li");
-        $lineItem.remove();
-    });
 })
