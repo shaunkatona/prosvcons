54a1c1d173961f69aac9c17d5a1f01465cc13b1c
shaunkatona <shaun.katona@gmail.com>
2014-08-14 12:09:24 -0600

    added signup view to the login modal

--- server.js
+++ server.js
@@ -39,19 +39,4 @@
     app.listen(process.argv[2] || 80);
 
     exports = module.exports = app;
-
-    passport.use(new LocalStrategy(function(username, password, done){
-        Users.findOne({ username : username},function(err,user){
-            if(err) { return done(err); }
-            if(!user){
-                return done(null, false, { message: 'Incorrect username.' });
-            }
-
-            hash( password, user.salt, function (err, hash) {
-                if (err) { return done(err); }
-                if (hash == user.hash) return done(null, user);
-                done(null, false, { message: 'Incorrect password.' });
-            });
-        });
-    }));
 })();
